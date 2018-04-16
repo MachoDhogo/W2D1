f0dc88bdf9704892992c17910210b28f07bca8c6
MachoDhogo <chriswvliew@gmail.com>
2018-04-16 18:46:41 +0000

    Print in Chunks

---
+++ scriptnode.js
@@ -0,0 +1,25 @@
+//require http module
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOption = {
+    host: 'syntantris.github.io', //domain name
+    path: '/http-examples/step1.html' //path in domain name
+  };
+
+  //takes a callback with one parameter - response which is a stream that represents HTTP response
+  https.get(requestOption, function(response) {
+
+    //set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    //the callback is invoked when a `data` chunk received
+    //console.log each chunk, concatenate with a '\n'
+    response.on('data', function (data) {
+      console.log(data + '\n');
+    });
+  });
+}
+
+getAndPrintHTMLChunks();
